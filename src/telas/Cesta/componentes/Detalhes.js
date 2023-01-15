9ba9b38b1c370f50a19b8581306d9065b02b7bac
Emori <contatobrenoemori@gmail.com>
2023-01-15 13:18:52 -0300

    novo terminal

---
+++ src/telas/Cesta/componentes/Detalhes.js
@@ -0,0 +1,60 @@
+import React from "react";
+import { Image, StyleSheet, Dimensions, View } from "react-native";
+
+import Texto from "../../../componentes/Texto";
+
+const width = Dimensions.get('screen').width;
+
+
+export default function Detalhes ({ nome, nomeFazenda, logoFazenda, descricao, preco }) {
+    return <>
+
+        <Texto style={estilos.nome}>{ nome }</Texto>
+        <View style={estilos.viewFazenda}>
+            <Image source={logoFazenda} style={estilos.imagemFazenda} />
+            <Texto style={estilos.nomeFazenda}>{nomeFazenda}</Texto>
+        </View>
+        <Texto style={estilos.descricao}> { descricao } </Texto>
+        <Texto style={estilos.preco}> { preco } </Texto>
+    </>
+    
+}
+
+const estilos = StyleSheet.create({
+    
+    cesta:{
+        paddingVertical: 8,
+        paddingHorizontal: 16,
+    },
+    nome: {
+        color:"#464646",
+        fontSize: 26,
+        lineHeight: 42,
+        fontWeight: "bold",
+    },
+    viewFazenda:{
+        paddingVertical: 12,
+        flexDirection: "row"
+    },
+    nomeFazenda: {
+        fontSize: 16,
+        lineHeight: 26,
+    },
+    imagemFazenda:{
+        width: 32,
+        height: 32,
+        marginRight: 12
+    },
+    descricao: {
+        color: "#A3A3A3",
+        fontSize: 16,
+        lineHeight: 26
+    },
+    preco: {
+        color: "#2A9F85",
+        fontWeight: "bold",
+        fontSize: 26,
+        lineHeight: 42,
+        marginTop: 8
+    }
+});
